4e4643fa924d9cfe1ae45301bf0e07cebcb5d02b
Sulfurous-Impersonation <june@watson-net.com>
2022-11-14 23:34:06 -0800

    Updated dialogue test, added dialogue prefabs, cursor behavior

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    DialogueTrigger dialogueTrigger;
+    void Start()
+    {
+        dialogueTrigger = GetComponent<DialogueTrigger>();
+    }
+    void OnMouseDown()
+    {
+        print("triggering dialogue");
+        dialogueTrigger.TriggerDialogue();
+    }
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
