8691d78ee17ada7688e57d20356b036992135fe9
fquach <55416265+fquach@users.noreply.github.com>
2022-12-04 15:03:34 -0800

    added file header

--- Assets/Scripts/ButtonSFX.cs
+++ Assets/Scripts/ButtonSFX.cs
@@ -1,3 +1,13 @@
+/*** 
+*file: ButtonSFX.cs 
+*Members: Juniper Watson
+*class: CS 4700 – Game Development 
+*assignment: program 4
+*date last modified: 12/3/2022 
+* 
+*purpose: This script plays the sound effects of buttons as they are clicked.
+* 
+**/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
