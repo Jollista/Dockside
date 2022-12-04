253b8ab8760c820e372f44071a87919b0cf0bd76
fquach <55416265+fquach@users.noreply.github.com>
2022-12-04 14:50:46 -0800

    added file header

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,3 +1,14 @@
+/*** 
+*file: SceneTransition.cs 
+*Members: Juniper Watson
+*class: CS 4700 – Game Development 
+*assignment: program 4
+*date last modified: 11/20/2022 
+* 
+*purpose: This scripts represents and handles the text area where dialogue is displayed 
+*within the actual dialogue box on screen. 
+* 
+**/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
