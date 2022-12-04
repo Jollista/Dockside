2da3a232cd0761d76a9fe1e4db55a74777c6a268
fquach <55416265+fquach@users.noreply.github.com>
2022-12-04 14:45:24 -0800

    added file header

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -1,3 +1,14 @@
+/*** 
+*file: SceneTransition.cs 
+*Members: Andrew Sanford
+*class: CS 4700 – Game Development 
+*assignment: program 4
+*date last modified: 11/28/2022 
+* 
+*purpose: This scripts ensures the buttons on the main menu 
+*while perform the correct action when clicked.
+* 
+**/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
