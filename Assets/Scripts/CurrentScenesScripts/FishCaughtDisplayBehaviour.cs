530d130d16d37a3f193b6a4e28e2d03df37e1009
fquach <55416265+fquach@users.noreply.github.com>
2022-12-04 15:05:34 -0800

    added file header

--- Assets/Scripts/CurrentScenesScripts/FishCaughtDisplayBehaviour.cs
+++ Assets/Scripts/CurrentScenesScripts/FishCaughtDisplayBehaviour.cs
@@ -1,3 +1,14 @@
+/*** 
+*file: FishCaughtDisplayBehaviour.cs 
+*Members: Andrew Sanford
+*class: CS 4700 – Game Development 
+*assignment: program 4
+*date last modified: 12/27/2022 
+* 
+*purpose: This script handles displaying the popup menu that displays what the player caught
+*from a fishing attempt. 
+* 
+**/
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
